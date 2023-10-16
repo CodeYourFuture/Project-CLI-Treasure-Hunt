bab8b51dc34e8041ed2d0b32c69b56881bfbc8bb
elmira <elmira.zangeneh89@gmail.com>
2023-10-16 23:10:48 +0100

    complete#

--- Clues/Who/help.js
+++ Clues/Who/help.js
@@ -1,4 +1,4 @@
 console.log(`How do you move a file? 
 Try typing mv into the command line. 
-What happens? 
+What happens? move and rename the file
 What should you google to figure out the right command?`)
