f08d8201bfb5ea354bba16768aa013d37fb4f9f0
Fathi <fhkahin@gmail.com>
2023-10-19 11:11:56 +0100

    NW6 | Fthi_Kahin | CLI-Treasure-Hunt | Week 2

--- Clues/What/clue.js
+++ Clues/What/clue.js
@@ -4,3 +4,9 @@ touch answer.txt
 2. Check this by listing all the files in this folder:
 ls -a
 2. Now change directory to the Who folder and find the answer`)
+All files
+1. clues;
+2. gitignore;
+3. clue.js;
+4. license;
+5. readme.md;
